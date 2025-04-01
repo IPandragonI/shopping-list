e9dff7c3d5b9356a224df56eb3e940426a92e846
IPandragonI <mathysf84pro@gmail.com>
2025-04-01 20:38:55 +0200

    Fin étape 1

--- src/main.js
+++ src/main.js
@@ -16,58 +16,71 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    const createProductElement = (product, list) => {
+        const li = document.createElement('li');
+        li.classList.add('card', 'shadow-sm', 'p-6', 'mb-5', 'bg-white', 'rounded-xl', 'hover:shadow-lg', 'transition-shadow', 'duration-300');
+
+        const quantityTaken = list.find(item => item.nom === product.nom)?.quantite || 0;
+        const quantityCount = product.quantite_stock - quantityTaken;
+
+        const title = document.createElement('h2');
+        const quantity = document.createElement('p');
+        const price = document.createElement('p');
+        const addToList = document.createElement('button');
+
+        title.innerHTML = `<h2 class="card-title my-1 text-xl">${product.nom}</h2>`;
+        quantity.innerHTML = `<p class="my-1"><strong>Quantité : </strong>${quantityCount}</p>`;
+        price.innerHTML = `<p class="my-1"><strong>Prix : </strong>${product.prix_unitaire}€</p>`;
+        addToList.innerHTML = `<button type="button" class="btn btn-secondary mt-5" data-id="${product.nom}">Ajouter à la liste</button>`;
+
+        if (quantityCount === 0) {
+            li.classList.add('opacity-75');
+            addToList.disabled = true;
+            addToList.innerHTML = '<button type="button" class="btn btn-secondary mt-5" data-id="${product.nom}" disabled>Produit épuisé</button>';
+        }
+
+        addToList.addEventListener('click', () => handleAddToList(product, addToList, quantity, li));
+
+        li.appendChild(title);
+        li.appendChild(quantity);
+        li.appendChild(price);
+        li.appendChild(addToList);
+
+        return li;
+    };
+
     const renderProducts = (products) => {
         const list = JSON.parse(localStorage.getItem('list')) || [];
         productList.innerHTML = '';
         productNumber.innerHTML = `<p class="text-center">${products.length} produits</p>`;
         products.forEach(product => {
-            const li = document.createElement('li');
-            li.classList.add('card', 'shadow-sm', 'p-6', 'mb-5', 'bg-white', 'rounded-xl', 'hover:shadow-lg', 'transition-shadow', 'duration-300');
-            const quantityTaken = list.find(item => item.nom === product.nom)?.quantite || 0;
-            const quantityCount = product.quantite_stock - quantityTaken;
-            const addToList = document.createElement('button')
-            if (quantityCount === 0) {
-                li.classList.add('opacity-50');
-                addToList.disabled = true;
-            }
-            const title = document.createElement('h2');
-            const quantity = document.createElement('p');
-            const price = document.createElement('p');
-
-            title.innerHTML = `<h2 class="card-title my-1 text-xl">${product.nom}</h2>`;
-            quantity.innerHTML = `<p class="my-1"><strong>Quantité : </strong>${quantityCount}</p>`;
-            price.innerHTML = `<p class="my-1"><strong>Prix : </strong>${product.prix_unitaire}€</p>`;
-            addToList.innerHTML = `<button type="button" class="btn btn-secondary mt-5" data-id="${product.nom}">Ajouter à la liste</button>`;
-
-            addToList.addEventListener('click', () => {
-                const list = JSON.parse(localStorage.getItem('list')) || [];
-                const productInCart = list.find(item => item.nom === product.nom);
-                if (productInCart) {
-                    productInCart.quantite = parseInt(productInCart.quantite) + 1;
-                } else {
-                    list.push({...product, quantite: 1});
-                }
-                localStorage.setItem('list', JSON.stringify(list));
-                addToList.innerHTML = '<button type="button" class="btn btn-success mt-5" data-id="${product.nom}">Produit ajouté !</button>';
-                setTimeout(() => {
-                    addToList.innerHTML = `<button type="button" class="btn btn-secondary mt-5" data-id="${product.nom}">Ajouter à la liste</button>`;
-                }, 2000);
-                const quantityTaken = list.find(item => item.nom === product.nom)?.quantite || 0;
-                const quantityCount = product.quantite_stock - quantityTaken;
-                quantity.innerHTML = `<p class="my-1"><strong>Quantité : </strong>${quantityCount}</p>`;
-
-                if (quantityCount === 0) {
-                    li.classList.add('opacity-50');
-                    addToList.disabled = true;
-                }
-            });
-
-            li.appendChild(title);
-            li.appendChild(quantity);
-            li.appendChild(price);
-            li.appendChild(addToList);
-            productList.appendChild(li);
+            const productElement = createProductElement(product, list);
+            productList.appendChild(productElement);
         });
+    };
+
+    const handleAddToList = (product, addToList, quantity, li) => {
+        const list = JSON.parse(localStorage.getItem('list')) || [];
+        const productInCart = list.find(item => item.nom === product.nom);
+        if (productInCart) {
+            productInCart.quantite = parseInt(productInCart.quantite) + 1;
+        } else {
+            list.push({ ...product, quantite: 1 });
+        }
+        localStorage.setItem('list', JSON.stringify(list));
+        addToList.innerHTML = '<button type="button" class="btn btn-active btn-primary mt-5" data-id="${product.nom}">Produit ajouté !</button>';
+        setTimeout(() => {
+            addToList.innerHTML = `<button type="button" class="btn btn-secondary mt-5" data-id="${product.nom}">Ajouter à la liste</button>`;
+        }, 500);
+        const quantityTaken = list.find(item => item.nom === product.nom)?.quantite || 0;
+        const quantityCount = product.quantite_stock - quantityTaken;
+        quantity.innerHTML = `<p class="my-1"><strong>Quantité : </strong>${quantityCount}</p>`;
+
+        if (quantityCount === 0) {
+            li.classList.add('opacity-75');
+            addToList.disabled = true;
+            addToList.innerHTML = '<button type="button" class="btn btn-secondary mt-5" data-id="${product.nom}" disabled>Produit épuisé</button>';
+        }
     };
 
     const filterProducts = (products, searchTerm) => {
