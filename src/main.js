159f8ad33cbad789ebc913fe906bffcee06a66fe
IPandragonI <mathysf84pro@gmail.com>
2025-04-24 10:53:51 +0200

    Gh Actions

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ export const localStorage = window.localStorage;
 
 export const fetchProducts = async () => {
     try {
-        const response = await fetch('/products-list.json');
+        const response = await fetch(`${import.meta.env.BASE_URL}/products-list.json`);
         return await response.json();
     } catch (error) {
         console.error('Error fetching products:', error);
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+
+// @ts-ignore
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
+    base: '/shopping-list/',
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                list: resolve(__dirname, 'list.html'),
+            },
+        }
+    },
     plugins: [
         tailwindcss(),
     ],
