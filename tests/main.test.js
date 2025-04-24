125861f4542b2cffdcef6f2897bb9cd364693351
IPandragonI <mathysf84pro@gmail.com>
2025-04-24 11:59:04 +0200

    Deployment

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,17 +1,17 @@
 import {describe, expect, test} from '@jest/globals';
-import {fetchTestProducts, filterProducts, sortProducts, handleAddToList} from '../src/main.js';
+import {fetchProducts, filterProducts, sortProducts, handleAddToList} from '../src/main.js';
 
 describe('Affichage des produits', () => {
     test('Retourne un tableau de produits', async () => {
-        const products = await fetchTestProducts();
+        const products = await fetchProducts();
         expect(Array.isArray(products)).toBe(true);
     });
     test('Retourne un tableau de produits non vide', async () => {
-        const products = await fetchTestProducts();
+        const products = await fetchProducts();
         expect(products.length).toBeGreaterThan(0);
     });
     test('Retourne un tableau de produits avec des objets', async () => {
-        const products = await fetchTestProducts();
+        const products = await fetchProducts();
         expect(products[0]).toEqual(expect.objectContaining({
             nom: expect.any(String),
             prix_unitaire: expect.any(Number),
@@ -19,7 +19,7 @@ describe('Affichage des produits', () => {
         }));
     });
     test('Retourne un tableau de 98 produits', async () => {
-        const products = await fetchTestProducts();
+        const products = await fetchProducts();
         expect(products.length).toBe(98);
     });
 });
