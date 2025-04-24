2de5278b005ae5d90e8a0a2fbf471f689fc519b5
IPandragonI <mathysf84pro@gmail.com>
2025-04-24 11:13:08 +0200

    Gh Actions

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ export const localStorage = window.localStorage;
 
 export const fetchProducts = async () => {
     try {
+        console.log(import.meta.env.BASE_URL);
         const response = await fetch(`${import.meta.env.BASE_URL}/products-list.json`);
         return await response.json();
     } catch (error) {
